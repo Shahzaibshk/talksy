f9ba373f5e21511a8de00240f91478cd0f7cc0d0
Shahzaibshk <ishahzaibali1472@gmail.com>
2025-06-25 15:43:20 -0700

    add firebase , set app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:talksy/app_theme.dart';
 import 'package:talksy/chat_screen.dart';
+import 'package:talksy/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,9 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.system,
       home: ChatScreen(),
     );
   }
